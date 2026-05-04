a458998190062c9437f857f3d207cbc8b46b480f
boyangqin <qboyang@gmail.com>
2026-05-04 00:24:02 -0700

    add some log for balance check

--- src/model/get_balance.rs
+++ src/model/get_balance.rs
@@ -1,6 +1,7 @@
 use std::time::{SystemTime, UNIX_EPOCH};
 use dioxus::prelude::ReadableExt;
 use reqwest::header::{HeaderMap, HeaderValue};
+use reqwest::Response;
 use serde::Deserialize;
 use crate::config::app_config::CONFIG;
 use crate::model::sign_signature::sign_request;
@@ -14,6 +15,7 @@ struct AccountInfo {
 struct AssetBalance {
     asset: String,
     free: String,
+    locked: String,
 }
 pub(crate) async fn get_balances() -> Result<(f64, f64, f64), Box<dyn std::error::Error>> {
     println!("🦀 Getting USDT balance...");
@@ -31,7 +33,7 @@ pub(crate) async fn get_balances() -> Result<(f64, f64, f64), Box<dyn std::error
     println!("🦀 url: {}", url);
     let mut headers = HeaderMap::new();
     headers.insert("X-MBX-APIKEY", HeaderValue::from_str(&app_config.api_key).unwrap());
-    let res = client.get(url).headers(headers).send().await?;
+    let res: Response = client.get(url).headers(headers).send().await?;
 
     // Check if the status is a success (200-299)
     if !&res.status().is_success() {
@@ -39,11 +41,13 @@ pub(crate) async fn get_balances() -> Result<(f64, f64, f64), Box<dyn std::error
         println!("❌ API Error Body: {}", error_text);
         return Err(format!("Binance API Error: ").into());
     }
-    println!("✅ Getting USDT balance success");
-    let account: AccountInfo = res.json().await?;
+    let body_text = res.text().await?;
+    println!("✅ RAW JSON FROM BINANCE: {}", body_text);
+    let account: AccountInfo = serde_json::from_str(&body_text)?;
+
     account.balances.iter().for_each(|b| {
-        if b.free.parse::<f64>().unwrap() == 0.0 {
-            println!("❌ asset {:?}", b);
+        if b.free.parse::<f64>().unwrap() == 0.0 && b.locked.parse::<f64>().unwrap() == 0.0{
+            // println!("❌ asset {:?}", b);
         } else {
             println!("✅ asset {:?}", b);
         }
